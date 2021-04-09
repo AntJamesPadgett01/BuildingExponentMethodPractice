883f06d1d2b6265fcd49b9b1629d862e8f1b77b7
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-08 21:17:49 -0500

    task completed

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BuildingExponentMethodPractice
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine(GetPow(4,3));
+        }
+
+        static int GetPow( int baseNum, int powNum)
+        {
+            int results = 1;
+
+            for (int i = 0; i < powNum; i++)
+            {
+                results = results * baseNum;
+            }
+
+            return results;
+        }
+    }
+}
